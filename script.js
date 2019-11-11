ed9c7df201487c56fb3cbbd3a214807150674323
Matthew Burtonshaw <MBurtonshaw@gmail.com>
2019-11-11 13:28:35 -0800

    Grammer in array changed; printQuote works now!

--- script.js
+++ script.js
@@ -12,29 +12,29 @@ project 1 - A Random Quote Generator
 ***/
 var quotes = [
     {quote : "The books that the world calls immoral are books that show the world its own shame. ",
-     source : "Oscar Wilde. ",
+     source : "Oscar Wilde",
      year : 1890,
-     citation : "'The Picture of Dorian Gray', written by Oscar Wilde. "},
+     citation : "'The Picture of Dorian Gray', written by Oscar Wilde"},
     
     {quote : "Nothing is so painful to the human mind as a great and sudden change. ",
-     source : "Mary Shelley. ",
+     source : "Mary Shelley",
      year : 1818,
      citation : "'Frankenstein, or the Modern Prometheus', written by Mary Shelley"},
     
     {quote : "Death has come to your little town, Sheriff. Now, you can either ignore it, or you can help me to stop it. ",
-     source : "Donald Pleasance as Dr. Loomis. ",
+     source : "Donald Pleasance as Dr. Loomis",
      year : 1978,
-     citation : "'Halloween', co-written by John Carpenter and Debra Hill. "},
+     citation : "'Halloween', co-written by John Carpenter and Debra Hill"},
     
     {quote : "What ain't no country I've ever heard of. They speak English in What? ",
-     source : "Samuel L. Jackson as Jules Winnfield. ",
+     source : "Samuel L. Jackson as Jules Winnfield",
      year : 1994,
-     citation : "'Pulp Fiction', written and directed by Quentin Tarantino. "},
+     citation : "'Pulp Fiction', written and directed by Quentin Tarantino"},
     
     {quote : "This is not 'Nam. This is bowling. There are rules. ",
-     source : "John Goodman as Walter Sobchak. ",
+     source : "John Goodman as Walter Sobchak",
      year : 1998,
-     citation : "'The Big Lebowski', written and directed by Joel and Ethan Coen. "}
+     citation : "'The Big Lebowski', written and directed by Joel and Ethan Coen"}
 ];
 
 /***
@@ -60,7 +60,7 @@ function printQuote() {
     for (var i = 0; i < quotes.length; i++) {
         let  quoteVar = getRandomQuote();
         let quoteBox = document.getElementById("quote-box");
-        quoteBox.innerHTML = quoteVar.quote + quoteVar.source + quoteVar.citation + quoteVar.year;
+        quoteBox.innerHTML = "<p class='quote'>" + quoteVar.quote + "</p>" + "<p class='source'>" + quoteVar.source + "<span class='citation'>" +  quoteVar.citation + "</span>" + "<span class='year'>" + quoteVar.year + "</span>" + "</p>";
     }
 }
 
