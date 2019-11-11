c374f8dae476d7b2c8f789f6f714b399dd420f3e
Matthew Burtonshaw <MBurtonshaw@gmail.com>
2019-11-11 13:48:01 -0800

    Added extra properties to objects

--- script.js
+++ script.js
@@ -14,27 +14,37 @@ var quotes = [
     {quote : "The books that the world calls immoral are books that show the world its own shame. ",
      source : "Oscar Wilde",
      year : 1890,
-     citation : "'The Picture of Dorian Gray', written by Oscar Wilde"},
+     citation : "'The Picture of Dorian Gray', written by Oscar Wilde",
+     medium : "Novel",
+     country : "England"},
     
     {quote : "Nothing is so painful to the human mind as a great and sudden change. ",
      source : "Mary Shelley",
      year : 1818,
-     citation : "'Frankenstein, or the Modern Prometheus', written by Mary Shelley"},
+     citation : "'Frankenstein, or the Modern Prometheus', written by Mary Shelley",
+     medium : "Novel",
+     country : "England"},
     
     {quote : "Death has come to your little town, Sheriff. Now, you can either ignore it, or you can help me to stop it. ",
      source : "Donald Pleasance as Dr. Loomis",
      year : 1978,
-     citation : "'Halloween', co-written by John Carpenter and Debra Hill"},
+     citation : "'Halloween', co-written by John Carpenter and Debra Hill",
+     medium : "Film",
+     country : "United States"},
     
     {quote : "What ain't no country I've ever heard of. They speak English in What? ",
      source : "Samuel L. Jackson as Jules Winnfield",
      year : 1994,
-     citation : "'Pulp Fiction', written and directed by Quentin Tarantino"},
+     citation : "'Pulp Fiction', written and directed by Quentin Tarantino",
+     medium : "Film",
+     country : "United States"},
     
     {quote : "This is not 'Nam. This is bowling. There are rules. ",
      source : "John Goodman as Walter Sobchak",
      year : 1998,
-     citation : "'The Big Lebowski', written and directed by Joel and Ethan Coen"}
+     citation : "'The Big Lebowski', written and directed by Joel and Ethan Coen",
+     medium : "Film",
+     country : "United States"}
 ];
 
 /***
@@ -58,7 +68,7 @@ function printQuote() {
     for (var i = 0; i < quotes.length; i++) {
         let  quoteVar = getRandomQuote();
         let quoteBox = document.getElementById("quote-box");
-        quoteBox.innerHTML = "<p class='quote'>" + quoteVar.quote + "</p>" + "<p class='source'>" + quoteVar.source + "<span class='citation'>" +  quoteVar.citation + "</span>" + "<span class='year'>" + quoteVar.year + "</span>" + "</p>";
+        quoteBox.innerHTML = "<p class='quote'>" + quoteVar.quote + "</p>" + "<p class='source'>" + quoteVar.source + "<span class='citation'>" +  quoteVar.citation + "</span>" + "<span class='year'>" + quoteVar.year + "</span>" + "<span class ='citation'>" + quoteVar.medium + "</span>" + "<span class ='citation'>" + quoteVar.country + "</span>" + "</p>";
     }
 }
 
